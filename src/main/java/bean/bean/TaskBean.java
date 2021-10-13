37ec6c5f0add441fa9d9c4d4d58d0fac11ad4b84
Brenda Silva <brendaalexandra1213@gmail.com>
2021-10-13 19:11:52 -0300

    Task xhtml in progress

--- src/main/java/bean/bean/TaskBean.java
+++ src/main/java/bean/bean/TaskBean.java
@@ -10,7 +10,10 @@ import javax.inject.Named;
 import org.omnifaces.util.Messages;
 
 import dao.TaskDAO;
+import domain.Employee;
 import domain.Task;
+import enums.PriorityEnum;
+import enums.StatusEnum;
 
 @Named
 @ViewScoped
@@ -22,12 +25,26 @@ public class TaskBean implements Serializable{
 	private List<Task> tasks;
 	private TaskDAO taskDAO;
 	
+	private List<Employee> employees;
+	private PriorityEnum[] priorities;
+	private StatusEnum[] listStatus;
+	
 	public TaskBean() {
 	}
 	
 	public TaskBean(Task task, TaskDAO taskDAO) {
 		this.task = task;
 		this.taskDAO = taskDAO;
+	}
+	
+	public TaskBean(Task task, List<Task> tasks, TaskDAO taskDAO, List<Employee> employees, PriorityEnum[] priorities,
+			StatusEnum[] listStatus) {
+		this.task = task;
+		this.tasks = tasks;
+		this.taskDAO = taskDAO;
+		this.employees = employees;
+		this.priorities = priorities;
+		this.listStatus = listStatus;
 	}
 
 	public Task getTask() {
@@ -45,11 +62,46 @@ public class TaskBean implements Serializable{
 	public void setTaskDAO(TaskDAO taskDAO) {
 		this.taskDAO = taskDAO;
 	}
+	
+	public List<Task> getTasks() {
+		return tasks;
+	}
+
+	public void setTasks(List<Task> tasks) {
+		this.tasks = tasks;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	public PriorityEnum[] getPriorities() {
+		return priorities;
+	}
+
+	public void setPriorities(PriorityEnum[] priorities) {
+		this.priorities = priorities;
+	}
+
+	public StatusEnum[] getListStatus() {
+		return listStatus;
+	}
+
+	public void setListStatus(StatusEnum[] listStatus) {
+		this.listStatus = listStatus;
+	}
 
 	@PostConstruct
 	public void init() {
 		this.task = new Task();
 		this.taskDAO = new TaskDAO();
+		
+		this.priorities = PriorityEnum.values();
+		this.listStatus = StatusEnum.values();
 	}
 	
 	public void saveTask() {
